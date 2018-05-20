996f4535b223e65dbd58285b93a8a33e60b9f938
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2018-05-19 23:21:12 -0300

    add rank, with change insert and storage

--- ranking.cpp
+++ ranking.cpp
@@ -6,10 +6,13 @@ struct rank
 {
 	string name_user;
 	int score;
-} storage_easy[3], storage_medium[3], storage_hard[3];
+};
 
-// ta meio seboso ainda no else, mas tava dando problema no ponteiro, dps mudo
-rank* get_rank(int difficulty)
+vector<rank> storage_easy;
+vector<rank> storage_medium;
+vector<rank> storage_hard;
+
+vector<rank> get_rank(int difficulty)
 {
 	if(difficulty == 1)
 	{
@@ -19,7 +22,7 @@ rank* get_rank(int difficulty)
 	{
 		return storage_medium;
 	}
-	else
+	else if(difficulty == 3)
 	{
 		return storage_hard;
 	}
@@ -27,41 +30,63 @@ rank* get_rank(int difficulty)
 
 void see_score(int difficulty)
 {
-	rank *rank_selected = get_rank(difficulty);
 
-	string show;
-	for(int i = 0; i < 3; i++)
+	vector<rank> get_vector = get_rank(difficulty);
+
+	for(int i = 0; i < get_vector.size(); i++)
 	{
-		if(rank_selected[i].name_user != "" && rank_selected[i].score != 0)
-		{
-			cout << "score atual " << rank_selected[i].name_user << " " << rank_selected[i].score << endl;
-		}
+		cout << "score atual " << get_vector[i].name_user << " " << get_vector[i].score << endl;
 	}
 
 }
-
 
 bool operator<(const rank& r1, const rank& r2)
 {
 	return r1.score < r2.score;
 }
 
-void sort_rank(int difficulty)
+void sort_vector()
+{
+	sort(storage_easy.begin(), storage_easy.end());
+	sort(storage_medium.begin(), storage_medium.end());
+	sort(storage_hard.begin(), storage_hard.end());
+}
+
+void insert(int difficulty, string name, int new_score)
 {
 
-	rank *rank_selected = get_rank(difficulty);
+	rank r_insert;
+	r_insert.name_user = name;
+	r_insert.score = new_score;
 
-	sort(rank_selected, rank_selected + 3);
+	if(difficulty == 1)
+	{
+		storage_easy.push_back(r_insert);
+	}
+	else if(difficulty == 2)
+	{
+		storage_medium.push_back(r_insert);
+	}
+	else if(difficulty == 3)
+	{
+		storage_hard.push_back(r_insert);
+	}
+
+	sort_vector();
 
 }
 
-void insert(int difficulty, rank r)
+void see_three_highest_score(int difficulty)
 {
-	rank *rank_selected = get_rank(difficulty);
 
-	// inserir dando shift nas posicoes, lembrar (era o erro)
+	int count = 1;
 
-	sort_rank(difficulty);
+	while (count != 4)
+	{
+		cout << get_rank(difficulty)[get_rank(difficulty).size() - count].name_user << " " << get_rank(difficulty)[get_rank(difficulty).size() - count].score << endl;
+		count++;
+	}
+
 
 }
 
@@ -73,16 +98,24 @@ int main(int argc, char** argv)
 
 	r1.score = 100;
 
-	insert(1, r1);
+	insert(1, r1.name_user, r1.score);
 
 	rank r2;
 
 	r2.score = 101;
 	r2.name_user = "ls";
 
-	insert(1, r2);
+	insert(1, r2.name_user, r2.score);
 
-	see_score(1);
+	insert(1, "oi", 1000);
+
+	insert(1, "blabla", 102);
+
+	insert(1, "hiago2", 100);
+
+	//see_score(1);
+
+	see_three_highest_score(1);
 
 	return 0;
 }
