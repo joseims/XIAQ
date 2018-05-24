8a25e200681fc035caad30852bf14933f8e2b6a7
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2018-05-23 23:23:00 -0300

    Fixing the game's flow

--- game.cpp
+++ game.cpp
@@ -3,35 +3,77 @@
 #include "monster-generator.cpp"
 #include "battle.cpp"
 #include "store.cpp"
+#include "ranking.cpp"
 
 using namespace std;
 
 const string INTRODUCTION = "BEM VINDO AO XIAQ\n";
 const string CONGRATULATIONS_MESSAGE = "Parabéns!! Você ganhou mais uma batalha..\n";
-const string OPTIONS_MESSAGE = "Digite 1 caso deseje ir a loja ou 1 se quiser continuar batalhando.\n";
-const string LOSS_MESSAGE = "Infelizmente chegamos ao fim após essa derrota.\n";
+const string START_OPTIONS_MESSAGE = "Digite 1 caso queira batalhar, 2 se quiser ver as instruções ou 3 para visualizar o ranking.\n";
+const string IN_GAME_OPTIONS_MESSAGE = "Digite 2 caso deseje ir a loja ou 1 se quiser continuar batalhando.\n";
+const string LOSS_MESSAGE = "Infelizmente chegamos ao fim dessa partida após essa derrota.\n";
+const string INSTRUCTIONS = "\n";
 
-void start_game(main_character character, monster enemy) {
+void play(main_character &character) {
   //Select the difficult level before start the game.
-  while(battle_workflow(character, enemy)) {
-    printf("%s", CONGRATULATIONS_MESSAGE.c_str());
-    printf("%s", OPTIONS_MESSAGE.c_str());
+  int battles_counter = 0;
+  int still_in_battle = 0;
 
-    int option;
+  do {
+    if(still_in_battle)
+      printf("%s", CONGRATULATIONS_MESSAGE.c_str());
+      character.coins ++;
+
+    if(battles_counter == 5) {
+      printf("%s", IN_GAME_OPTIONS_MESSAGE.c_str());
+
+      int option;
+      scanf("%d", &option);
+      if(option == 2) {
+        see_store(character);
+      }
+      battles_counter = 0;
+    }
+
+    monster enemy = generate_monster(false, ++character.game_progress_multiplier);
+    still_in_battle = battle_workflow(character, enemy);
+    battles_counter++;
+  } while(still_in_battle);
+
+  printf("%s", LOSS_MESSAGE.c_str());
+}
+
+void game_setup(main_character &character) {
+  generate_all_alterers();
+  generate_all_monsters();
+  character.game_progress_multiplier = 1;
+}
+
+void start_menu(main_character &character) {
+  printf("%s", INTRODUCTION.c_str());
+  int option;
+  while(1) {
+    printf("%s", START_OPTIONS_MESSAGE.c_str());
     scanf("%d", &option);
-    if(option == 2) {
-      see_store(character);
+
+    switch(option) {
+      case 1:
+        game_setup(character);
+        play(character);
+        break;
+      case 2:
+        printf("%s", INSTRUCTIONS.c_str());
+        break;
+      case 3:
+        see_all_score(1);
+        break;
+      default:
+        printf("Opção inválida");
+        break;
     }
   }
 }
 
 int main() {
-  printf("%s", INTRODUCTION.c_str());
-
-  monster enemy = get_random_monster();
-  generate_generic_monster(&enemy);
-  start_game(character, enemy);
-
-  //Refresh the rank here, after the game's ending.
-  printf("%s", LOSS_MESSAGE.c_str());
+  start_menu(character);
 }
