a361760385179d1f0fb6282d8d6a15a0276bd607
Raoni Smaneoto <raoni.smaneoto@ccc.ufcg.edu.br>
2018-05-24 21:15:41 -0300

    Creating log's module

---
+++ log.cpp
@@ -0,0 +1,38 @@
+#ifndef OG_CPP
+#define LOG_CPP
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string LOG = "";
+
+const string FILENAME = "logs.txt";
+
+void add_log(string current_log) {
+  LOG += current_log;
+}
+
+void save_log() {
+  ofstream file;
+  file.open(FILENAME);
+  file << LOG;
+  file.close();
+}
+
+void load_log() {
+  LOG = "";
+  string line;
+  ifstream file(FILENAME);
+  if(file.is_open()) {
+    while(getline(file, line)) {
+      LOG += line + "\n";
+    }
+    file.close();
+  }
+}
+
+void print_log() {
+  cout << LOG << endl;
+}
+
+#endif
